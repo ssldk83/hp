f047ad6bc3d01d1743d5ee801f5941cd7d758bea
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-08-02 22:55:18 +0200

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,75 +1,58 @@
-from tespy.networks import Network
-from tespy.components import Source, Sink, Pump, HeatExchanger, Compressor, Valve, CycleCloser
+from tespy.components import (Sink, Source, Pump, HeatExchanger, Compressor, Valve,
+                               CycleCloser)
 from tespy.connections import Connection
+from tespy.networks import Network
+from tespy.tools.characteristics import CharLine
 import numpy as np
 
-
-def run_simulation(working_fluid, sink_return, sink_supply, source_in, source_out):
-    fluids = [working_fluid, "water"]
-    nw = Network(T_unit='C', p_unit='bar', h_unit='kJ / kg', fluids=fluids)
+def run_simulation(fluid, t_sink_in, t_sink_out, t_source_in, t_source_out):
+    # Set up the network
+    fluids = [fluid, "water"]
+    nw = Network(fluids=fluids, T_unit='C', p_unit='bar', h_unit='kJ / kg', m_unit='kg / s')
 
     # Components
-    # Refrigerant side
-    cc = CycleCloser("cycle closer")
-    src_r = Source("refrigerant source")
-    evap = HeatExchanger("evaporator")
-    comp = Compressor("compressor")
-    cond = HeatExchanger("condenser")
-    valve = Valve("valve")
+    # Refrigerant loop
+    cc = CycleCloser('cycle closer')
+    evap = HeatExchanger('evaporator')
+    comp = Compressor('compressor')
+    cond = HeatExchanger('condenser')
+    valve = Valve('expansion valve')
 
-    # Sink side (district heating)
-    src_sink = Source("district return")
-    pump_sink = Pump("sink pump")
-    sink = Sink("district supply")
+    # Source/sink loop
+    src_w = Source('wastewater source')
+    sink_w = Sink('wastewater sink')
+    src_dh = Source('district return')
+    sink_dh = Sink('district supply')
 
-    # Source side (wastewater)
-    src_source = Source("wastewater inlet")
-    pump_source = Pump("source pump")
-    sink_source = Sink("wastewater outlet")
-
-    # Refrigerant cycle connections
-    c0 = Connection(src_r, "out1", evap, "in1", label="0")
+    # Connections: Refrigerant cycle
+    c0 = Connection(cc, "out1", evap, "in1", label="0")
     c1 = Connection(evap, "out1", comp, "in1", label="1")
     c2 = Connection(comp, "out1", cond, "in1", label="2")
     c3 = Connection(cond, "out1", valve, "in1", label="3")
     c4 = Connection(valve, "out1", cc, "in1", label="4")
-    c5 = Connection(cc, "out1", evap, "in1", label="5")
 
-    # Sink side connections
-    c20 = Connection(src_sink, "out1", cond, "in2", label="20")
-    c21 = Connection(cond, "out2", pump_sink, "in1", label="21")
-    c22 = Connection(pump_sink, "out1", sink, "in1", label="22")
+    # Connections: Source side (wastewater)
+    c10 = Connection(src_w, "out1", evap, "in2", label="10")
+    c11 = Connection(evap, "out2", sink_w, "in1", label="11")
 
-    # Source side connections
-    c10 = Connection(src_source, "out1", pump_source, "in1", label="10")
-    c11 = Connection(pump_source, "out1", evap, "in2", label="11")
-    c12 = Connection(evap, "out2", sink_source, "in1", label="12")
+    # Connections: Sink side (district heating)
+    c20 = Connection(src_dh, "out1", cond, "in2", label="20")
+    c21 = Connection(cond, "out2", sink_dh, "in1", label="21")
 
     # Add to network
-    nw.add_conns(c0, c1, c2, c3, c4, c5,
-                 c10, c11, c12,
-                 c20, c21, c22)
+    nw.add_conns(c0, c1, c2, c3, c4, c10, c11, c20, c21)
 
-    # Fluid settings
-    c0.set_attr(fluid={working_fluid: 1})
-    c5.set_attr(fluid={working_fluid: 1})
-    c10.set_attr(T=source_in, fluid={"water": 1})
-    c12.set_attr(T=source_out)
-    c20.set_attr(T=sink_return, fluid={"water": 1})
-    c22.set_attr(p=1.013)
-    c21.set_attr(T=sink_supply)
+    # Parametrize
+    c10.set_attr(T=t_source_in, fluid={"water": 1})
+    c11.set_attr(T=t_source_out)
+    c20.set_attr(T=t_sink_in, fluid={"water": 1})
+    c21.set_attr(T=t_sink_out)
+    c0.set_attr(fluid={fluid: 1})
 
-    # Pressures
-    c1.set_attr(p=8)
-    c3.set_attr(p=3)
+    comp.set_attr(eta_s=0.8)
 
-    # Efficiencies
-    comp.set_attr(eta_s=0.85)
-    pump_sink.set_attr(eta_s=0.8)
-    pump_source.set_attr(eta_s=0.8)
-
-    # Run simulation
+    # Solve
     nw.solve(mode="design")
-    cop = cond.Q.val / comp.P.val
+    cop = c2.h.val / (c2.h.val - c1.h.val)
 
     return round(cop, 2), nw.res
